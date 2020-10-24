f43df02fe081ea6bd4b71db837b5c32e347572b2
Odin <odin.chiang@gmail.com>
2020-10-24 19:23:06 +0800

    加入專案檔案。

---
+++ MyCourseManagement/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace MyCourseManagement
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
