6ae3aa07c3ce9e1fcb8ba7cc59f75cec6f341fc9
Odin <odin.chiang@gmail.com>
2020-10-26 20:59:55 +0800

    登入介面-關閉視窗，命令綁定。

---
+++ MyCourseManagement/Common/CommandBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MyCourseManagement.Common
+{
+    public class CommandBase : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public Action<object> DoExecute { get; set; }
+        public Func<object, bool> DoCanExecute { get; set; }
+
+        public bool CanExecute(object parameter)
+        {
+            return DoCanExecute?.Invoke(parameter) == true;
+        }
+
+        public void Execute(object parameter)
+        {
+            DoExecute?.Invoke(parameter);
+        }
+    }
+}
---
+++ MyCourseManagement/ViewModels/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using MyCourseManagement.Common;
+
+namespace MyCourseManagement.ViewModels
+{
+    public class LoginViewModel
+    {
+        public CommandBase CloseWindowCommand { get; set; }
+
+        public LoginViewModel()
+        {
+            CloseWindowCommand = new CommandBase()
+            {
+                DoExecute = x =>
+                {
+                    (x as Window).Close();
+                },
+                DoCanExecute = x => true
+            };
+        }
+    }
+}
